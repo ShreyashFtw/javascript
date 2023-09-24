f7b06168c65c44f963ad4491cc5c998d39baa23e
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2023-09-24 20:24:09 +0000

    string

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,27 @@
+const name = "Shreyash";
+const repoCount = 50;
+
+// console.log(name + repoCount +"Value");
+
+console.log(`Hello my name is ${name} and my repoCOunt is ${repoCount}`);
+
+const gameName = new String ('shreyash-er-er');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+const newString = "      shreyash     ";
+console.log(newString);
+console.log(newString.trim());
+
+const url ="https://shreyash.com/shreyash%20thaware"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('shre'));
+
+console.log(gameName.split('-'));
