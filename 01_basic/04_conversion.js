366f04f1525dd662f20f5deb7d1226703178fb5a
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2023-09-24 19:42:43 +0000

    conversion

---
+++ 01_basic/04_conversion.js
@@ -0,0 +1,16 @@
+console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log( 2!= 1);
+
+ console.log( "2" > 1);
+// console.log( "02" > 1);
+
+console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0)
+
+console.log(undefined == 0);//always false we can avoid this type of conversion
+
+//=== strict check
+
+console.log("2" === 2);//datatype not match no conversion
