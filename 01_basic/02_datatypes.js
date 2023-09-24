bb85db15801399cc8a84ebfd72a981576315f310
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2023-09-24 18:51:27 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version or standard "not necessary to use"
+
+//alert(3 + 3) //we are using nodejs,not browser
+
+console.log(3 + 3);
+
+/* Primitive Datatypes
+number => 2**32
+bigint
+string => " "
+boolean=> true/false
+null => standalone value
+undefined => value not assigned
+symbol => unique most used in react for components
+
+*object
+
+
+*/
+console.log(typeof "shreyash");
+console.log(typeof null);//null is object - output(undefined) 
+console.log(typeof undefined);
