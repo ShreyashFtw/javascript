9d31a6b1147cff3a69274f0868c4462b6349719b
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2023-09-24 19:33:02 +0000

    commited 03

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -30,3 +30,44 @@ let someNumber = 33;
 let stringNumber= String(someNumber);
 
 console.log(stringNumber);//typeof is string
+
+/*************Operations*****************/
+
+let value = 3
+let negValue = -value;//assign negative value
+console.log(negValue);
+
+// console.log(2+3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1="Shreyash"
+let str2="Thaware"
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log(1 +"2");
+console.log("1" +"2");
+console.log("1"+ 2 + 2);
+console.log(1 + 2 +"3");
+
+/* postfix increment-If used postfix, with operator after operand (for example, x++), 
+the increment operator increments and returns the value before incrementing.
+
+let x=39;
+y=x++;
+console.log(x,y);
+O/P-40,39
+
+*prefix increment-If used prefix, with operator before operand (for example, ++x), 
+the increment operator increments and returns the value after incrementing.
+
+let x1=39;
+y=++x1;
+console.log(x,y);
+40,40*/
+
+let gameScore = 100;
+gameScore ++;
+console.log(gameScore);
