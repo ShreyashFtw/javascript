f50955eb3d4835d7cebaabb6fc859f1137df80b5
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2023-09-29 12:23:42 +0000

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh');
+
+((username) => 
+{
+    console.log(`${username}`);
+
+})('shreyash')
