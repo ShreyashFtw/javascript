f2c785e0bcb05974c064cefe6cca1434f619bd62
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2024-03-29 13:27:07 +0000

    7:57:07

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let i = 0
+while (i <= 10) {
+    // console.log(`Value of i is ${i}`);
+    i = i + 2;
+}
+
+let myArray = ["Superman", "Batman", "Ironman"];
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`Value of arr is ${myArray[arr]}`);
+    arr++;
+
+}
+let score = 1
+do {
+console.log(`Value of score is ${score}`);
+score++;
+} while (score <=10);
