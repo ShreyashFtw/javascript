7090946a0efc59e7ab70f85e6ed9b9afdaef52ed
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2024-03-30 13:11:17 +0000

    five

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: "JavaScript",
+    html: "HTML",
+    css: "CSS",
+    cpp: "C++",
+    python: "Python"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "html", "css", "cpp", "python"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    }
+    //forin cant be used on map also map is not iterable
+
