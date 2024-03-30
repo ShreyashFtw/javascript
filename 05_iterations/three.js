56f1ba51c19c3275f809607e7cbbb695a3bdfc9a
ShreyashFtw <145070148+ShreyashFtw@users.noreply.github.com>
2024-03-30 13:03:02 +0000

    three.js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -5,16 +5,36 @@
 
 const arr = [1, 2, 3, 4, 5]
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
 }
 
 const greetings = "Hello World"
 
 for (const greet of greetings) {
-    console.log(`Each character is ${greet}`);
+    // console.log(`Each character is ${greet}`);
 
 }
 
 //Maps
 
-const map = New Map()//MAp is a object
+const map = new Map()//Map is a object
+map.set("IN", "India")
+map.set("USA", "United States of America")
+map.set("UK", "United Kingdom")
+
+// console.log(map)
+for (const [abbrevations,country] of map) {//map has unique key-value pair and is ordered no duplicate allowed
+    console.log(abbrevations,"=>",country);
+    
+}
+
+
+const myObj={
+    name:"Shreyash",
+    age:20,
+}
+
+for (const [key,value] of myObj) {
+    // console.log(key,value);
+    // TypeError: myObj is not iterable
+}
